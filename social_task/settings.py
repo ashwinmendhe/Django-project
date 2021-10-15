d97c3f5e09c60cdfd689a1088096e618fa672a29
ashwinmendhe <75409973+ashwinmendhe@users.noreply.github.com>
2021-10-15 21:34:49 +0530

    1st commit

--- social_task/settings.py
+++ social_task/settings.py
@@ -79,14 +79,14 @@ WSGI_APPLICATION = 'social_task.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DEMO_TEST',
-        'USER':'postgres',
-        'PASSWORD':'1234',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'DEMO_TEST',
+        #'USER':'postgres',
+        #'PASSWORD':'1234',
+        #'HOST':'localhost',
+        #'PORT':'5432',
     }
 }
 
